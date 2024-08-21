49d8e307e725e9781df29b6a3dd59e6a23ef8c5e
wairesp <54559794+wairesp@users.noreply.github.com>
2024-08-21 11:52:03 -0500

    added search on column to interact with api nutrional information

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,8 @@ if ingredients_list:
     for element in ingredients_list:
         ingredients_string += element + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == element, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', element,' is ', search_on, '.')
 
         st.subheader(element + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + element)
