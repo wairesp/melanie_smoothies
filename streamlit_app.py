799b55abf9179db00250facc3b84a2123c323352
wairesp <54559794+wairesp@users.noreply.github.com>
2024-08-21 11:47:35 -0500

    added search on column to interact with api nutrional information

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 # st.write(session)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 
 name_on_order = st.text_input("Name on Smoothie:")
